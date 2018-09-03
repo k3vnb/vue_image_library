634032af5dbf1071d4e59ca4ee5fe3034fcb76ab
Kevin Boyle <papershack@gmail.com>
2018-09-03 13:19:26 -0700

    add basic store info, including action to set state to null

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,20 @@
-const state = {};
-const getters = {};
-const actions = {};
-const mutations = {};
+const state = {
+  token: null
+};
+
+//getters is ES6 shortform for const getters = { isLoggedIn: (state) => { return !!state.token}};
+const getters = {
+  isLoggedIn: state => !!state.token
+};
+
+const actions = {
+  logout: ({ commit }) => {
+    commit('setToken', null);
+  }
+};
+
+const mutations = {
+  setToken: (state, token) => {
+    state.token = token;
+  }
+};
